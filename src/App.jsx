9c34396a21224ad51b6a09922414ab5f02928cf2
Tran Duc Luong <luongtd08011998@gmail.com>
2026-03-30 10:39:32 +0700

    tesst lan 1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello world vs Luong</h1>
+      <h1>Hello world vs Luong dep trai</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
