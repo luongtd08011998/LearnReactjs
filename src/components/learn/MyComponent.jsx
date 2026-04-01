d0bd04fe63f0113609cc8210ca7651dab61b07f7
Tran Duc Luong <luongtd08011998@gmail.com>
2026-04-01 08:29:55 +0700

    ngay 01

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,19 @@
+import './style.css'
+
 const MyComponent = () => {
+    //const hoidanit = "Luong1";
+    //const hoidanit = 25;
+    //const hoidanit = [1 , 2 , 3]
+    const hoidanit = {
+        name: "hoidanit",
+        age: 25
+    }
   return (
-    <div>Luong hoc reactjs update</div>
+    <>
+    <div>{JSON.stringify(hoidanit)} hoc reactjs update</div>
+    <div>{console.log("ERIC")}</div>
+    <div className="child"> hay </div>
+    </>
   );
 }
 export default MyComponent;
